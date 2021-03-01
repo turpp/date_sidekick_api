9fd35fc8accd0618c1a0ca480a5edfd54886edd4
turpp <white.tripp@icloud.com>
2021-02-28 19:03:31 -0600

    added create action and user_params for user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,5 +25,20 @@ class UsersController < ApplicationController
     # def user_params
     #     params.require(:user).permit(:username, :password)
     # end
+   
     
+    # =============================
+
+    def create
+       user= User.new(user_params)
+       if user.save
+        render json:{status: 201, user:user, logged_in: true}
+       else
+        render json: {status: 500, message: 'There was an error in creating new account'}
+       end
+    end
+
+
+    private
+    params.require(:user).permit(:email, :password, :password_conformation)
 end
