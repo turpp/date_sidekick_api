59adbac31d9e6459f560b2e957518d48fa644e42
turpp <white.tripp@icloud.com>
2021-02-28 18:46:20 -0600

    initial setup for rails session with my api

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,29 +1,29 @@
 class UsersController < ApplicationController
-    skip_before_action :authorized, only: [:create]
+    # skip_before_action :authorized, only: [:create]
 
-    def create
-        byebug
-        @user=User.create(user_params)
-        if @user.valid?
-            @token = encode_token(user_id: @user.id)
-            render json: {user: UserSerializer.new(@user), jwt: @token}, status: :created
-        else
-            render json: {error: 'failed to create user'}, status: :not_accaptable
-        end
-    end
+    # def create
+    #     byebug
+    #     @user=User.create(user_params)
+    #     if @user.valid?
+    #         @token = encode_token(user_id: @user.id)
+    #         render json: {user: UserSerializer.new(@user), jwt: @token}, status: :created
+    #     else
+    #         render json: {error: 'failed to create user'}, status: :not_accaptable
+    #     end
+    # end
 
-    def profile
-        render json: {user: UserSerializer.new(current_user)}, status: :accepted
-    end
+    # def profile
+    #     render json: {user: UserSerializer.new(current_user)}, status: :accepted
+    # end
 
     
     
     
     
-    private
+    # private
 
-    def user_params
-        params.require(:user).permit(:username, :password)
-    end
+    # def user_params
+    #     params.require(:user).permit(:username, :password)
+    # end
     
 end
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+
