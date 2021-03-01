8d02d3d8bdb97aae92fc6eb7ca470f4ab30c6a2f
turpp <white.tripp@icloud.com>
2021-02-28 20:36:19 -0600

    added creation of session when new session is created with user id

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,6 +4,7 @@ class SessionsController < ApplicationController
         user = User.find_by(username: params[:user][:username])
 
         if user&&user.authenticate(params[:user][:password])
+            session[:id] = user.id
             render json: {status: 201, user: user, logged_in: true}
         else
             render json: {status: 401, message: 'User not found or password incorrect'}
