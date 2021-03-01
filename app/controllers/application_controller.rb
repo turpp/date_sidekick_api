997a3945cfd5fb4602765e3822494529e5b9135f
turpp <white.tripp@icloud.com>
2021-02-28 20:35:43 -0600

    added helper methods to applicaiton controller for logged in and current user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -35,5 +35,15 @@ class ApplicationController < ActionController::Base
     # def authorized
     #     render json: {message: 'Please log in'}, status: :unauthorized unless logged_in?
     # end
+
+    skip_before_action :verify_authenticity_token
+    helper_method :logged_in, :current_user
     
+    def logged_in
+        !!current_user
+    end
+
+    def current_user
+        @current_user ||= User.find(session[:id]) if session[:id]
+    end
 end
