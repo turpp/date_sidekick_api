4b26efefcdfb744905d735643a74a13a5d27b8a8
turpp <white.tripp@icloud.com>
2021-03-01 21:08:50 -0600

    when doing customize food only add date to current date and save date to profile works and persist to datebase with correct realtionships

--- app/controllers/outings_controller.rb
+++ app/controllers/outings_controller.rb
@@ -1,2 +1,24 @@
 class OutingsController < ApplicationController
+
+    def create
+        byebug
+        user = User.find_by(id: params[:user][:id])
+        date=params[:date]
+        outing=Outing.new()
+        outing.user=user
+        activity=Activity.find_or_create_by(yelpID: params[:date][:id])
+        if activity.name == nil
+            activity.name = date[:name]
+            activity.img_url = date[:image_url]
+            activity.yelpUrl = date[:url]
+            activity.categories = date[:categories]
+            activity.price=date[:price]
+            activity.adress = date[:location][:display_address]
+            activity.phone = date[:display_phone]
+        end
+        outing.activities << activity
+        outing.save
+        render json: {status: 201}
+
+    end
 end
--- app/models/activity.rb
+++ app/models/activity.rb
@@ -2,7 +2,7 @@ class Activity < ApplicationRecord
     has_many :outing_activities
 
     has_many :outings, through: :outing_activities
-
+    serialize :categories
 
     # need to get my activity and outings realtionships to work
     
