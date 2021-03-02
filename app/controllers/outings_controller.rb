daffc4721c950e55a2744ecd1564fd21c981f9ab
turpp <white.tripp@icloud.com>
2021-03-02 04:32:37 -0600

    can now save date to account with the save date button on the current date card

--- app/controllers/outings_controller.rb
+++ app/controllers/outings_controller.rb
@@ -3,23 +3,64 @@ class OutingsController < ApplicationController
     def create
         # byebug
         user = User.find_by(id: params[:user][:id])
-        date=params[:date]
+
+        if params[:type]=='food'
+        food=params[:food]
         outing=Outing.new()
         outing.user=user
-        activity=Activity.find_or_create_by(yelpID: params[:date][:id])
+        activity=Activity.find_or_create_by(yelpID: params[:food][:id])
         if activity.name == nil
-            activity.name = date[:name]
-            activity.img_url = date[:image_url]
-            activity.yelpUrl = date[:url]
-            activity.categories = date[:categories]
-            activity.price=date[:price]
-            activity.adress = date[:location][:display_address]
-            activity.phone = date[:display_phone]
+            activity.name = food[:name]
+            activity.img_url = food[:image_url]
+            activity.yelpUrl = food[:url]
+            activity.categories = food[:categories]
+            activity.price=food[:price]
+            activity.adress = food[:location][:display_address]
+            activity.phone = food[:display_phone]
         end
         outing.activities << activity
         outing.save
         activity.save
         render json: {status: 201}
+        end
+
+        if params[:type]=='foodActivity'
+            food=params[:food]
+            activity=params[:activity]
+            outing=Outing.new()
+            outing.user=user
+            
+            dateFood=Activity.find_or_create_by(yelpID: params[:food][:id])
+            if dateFood.name == nil
+                dateFood.name = food[:name]
+                dateFood.img_url = food[:image_url]
+                dateFood.yelpUrl = food[:url]
+                dateFood.categories = food[:categories]
+                dateFood.price=food[:price]
+                dateFood.adress = food[:location][:display_address]
+                dateFood.phone = food[:display_phone]
+            end
+            outing.activities << dateFood
+
+            dateActivity=Activity.find_or_create_by(yelpID: params[:activity][:id])
+            if dateActivity.name == nil
+                dateActivity.name = activity[:name]
+                dateActivity.img_url = activity[:image_url]
+                dateActivity.yelpUrl = activity[:url]
+                dateActivity.categories = activity[:categories]
+                dateActivity.price=activity[:price]
+                dateActivity.adress = activity[:location][:display_address]
+                dateActivity.phone = activity[:display_phone]
+            end
+            outing.activities << dateActivity
+            outing.save
+            dateFood.save
+            dateActivity.save
+            render json: {status: 201}
+
+
+    
+        end
 
     end
 end
