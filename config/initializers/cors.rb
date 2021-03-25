8d09397d5a09d7a409f488acf0e96ab8df2d07ca
turpp <white.tripp@icloud.com>
2021-03-25 15:52:31 -0500

    changed url in cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -8,7 +8,7 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   # production
   allow do
-    origins 'https://glacial-harbor-67608.herokuapp.com'
+    origins 'https://date-sidekick.herokuapp.com'
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head],
